d1b623977b92079988ec0e3e4452f5763b13ac4d
Ranset <ransetfleites0@gmail.com>
2026-06-02 16:46:20 -0500

    refactor: migrate Firebase credentials to environment variables and remove hardcoded JSON files

--- modulos/notifications.py
+++ modulos/notifications.py
@@ -1,7 +1,24 @@
 import firebase_admin
 from firebase_admin import messaging, credentials
+import os
+from dotenv import load_dotenv
 
-cred = credentials.Certificate("modulos/remesa-admin-firebase-adminsdk-fbsvc-5b120ea7c6.json")
+load_dotenv()
+
+service_acount_info = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n") if os.getenv("FIREBASE_PRIVATE_KEY") else None,
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
+
+cred = credentials.Certificate(service_acount_info)
 firebase_admin.initialize_app(cred)
 
 def send_push_notification(token: str, title: str, body: str, data: dict = None):
@@ -26,5 +43,5 @@ def send_push_notification(token: str, title: str, body: str, data: dict = None)
 
 if __name__ == "__main__":
     # Example usage
-    test_token = "eKtGywUORbOvW3rD9nQG2D:APA91bEPG26Q4EWtM3tJmz_4mDmsd2y1GoOABnyu1JmwyufDRAeuZMXTtoiEcZxSU2I54eC5k4v4r5K5FBjlm_FU_y9vKGyPV-_Mrc7TWskJOBIeCcCvxQg"
+    test_token = "cf9-eV1j1vbytAy25Z16Oo:APA91bFMHi3Gl4WejglV9fNKIbYflTy_uMTDZj51KiXb32aUX1S5X53wIuziemUCbuvr3d-2IE1NMfVjhwcVY0uEkkIGYDuRohTKWKJN8sG7ucX4VZ31cpA"
     send_push_notification(test_token, "Notificación API", "Prueba desde local con Python", data={"key1": "value1", "key2": "value2"})
